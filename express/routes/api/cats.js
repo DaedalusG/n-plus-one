2e155fcb9d7ad7f3cfa62c27e64df0f5ce5107af
warrgiff <warrenbruceg@gmail.com>
2021-02-19 23:27:34 -0800

    messing around with the /owners route

--- express/routes/api/cats.js
+++ express/routes/api/cats.js
@@ -6,41 +6,41 @@ const { Cat, Toy } = require('../../db/models');
 const router = express.Router();
 
 router.get('/', asyncHandler(async function (req, res, next) {
-    const cats = await Cat.findAll();
-    res.json({ cats });
+  const cats = await Cat.findAll();
+  res.json({ cats });
 }));
 router.get('/owners', asyncHandler(async function (req, res, next) {
-    const cats = await Cat.findAll();
-    const result = []
-    for (cat of cats) {
-      result.push({
-        name: cat.name,
-        owner: await cat.getOwner(),
-        breed: cat.breed,
-        color: cat.color
-      });
-    }
-    res.json({ cats: result });
+  const cats = await Cat.findAll().include();
+  const result = []
+  for (cat of cats) {
+    result.push({
+      name: cat.name,
+      owner: await cat.getOwner(),
+      breed: cat.breed,
+      color: cat.color
+    });
+  }
+  res.json({ cats: result });
 }));
 router.get('/toys', asyncHandler(async function (req, res, next) {
-    const cats = await Cat.findAll();
-    const result = []
-    for (cat of cats) {
-      const toys = []
-      for (toy of await cat.getToys()) {
-        const toyType = await toy.getToyType();
-        toys.push({
-          name: toy.name,
-          type: toyType.name
-        })
-      }
-      result.push({
-        name: cat.name,
-        breed: cat.breed,
-        toys: toys,
-        color: cat.color
-      });
+  const cats = await Cat.findAll();
+  const result = []
+  for (cat of cats) {
+    const toys = []
+    for (toy of await cat.getToys()) {
+      const toyType = await toy.getToyType();
+      toys.push({
+        name: toy.name,
+        type: toyType.name
+      })
     }
-    res.json({ cats: result });
+    result.push({
+      name: cat.name,
+      breed: cat.breed,
+      toys: toys,
+      color: cat.color
+    });
+  }
+  res.json({ cats: result });
 }));
 module.exports = router;
